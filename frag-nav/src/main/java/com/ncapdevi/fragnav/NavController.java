58297a368a44c1ae502953673c9c9de84e7ac94b
Nic Capdevila <niccapdevila@Nics-MacBook-Air.local>
2016-03-27 11:46:50 -0700

    - bump the jam

--- frag-nav/src/main/java/com/ncapdevi/fragnav/NavController.java
+++ frag-nav/src/main/java/com/ncapdevi/fragnav/NavController.java
@@ -194,7 +194,7 @@ public class NavController {
         }
     }
 
-    //Todo handle other types of fragments 
+    //Todo handle other types of fragments
 /*    public void showBottomSheet(BottomSheetDialogFragment bottomSheetDialogFragment) {
         bottomSheetDialogFragment.show(mFragmentManager, bottomSheetDialogFragment.getClass().getName() + ++mTagCount);
     }*/

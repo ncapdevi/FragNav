d7a95c0dc59bd7984b042b8e12de1db44624594b
Nic Capdevila <niccapdevila@Nics-MacBook-Air.local>
2016-03-28 07:44:07 -0700

    - Better practice to name your tabs in your activity

--- app/src/main/java/com/ncapdevi/sample/MainActivity.java
+++ app/src/main/java/com/ncapdevi/sample/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
-import com.ncapdevi.fragnav.NavController;
+import com.ncapdevi.fragnav.FragNavController;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
 
@@ -14,7 +14,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements BaseFragment.FragmentNavigation {
     private BottomBar mBottomBar;
-    private NavController mNavController;
+    private FragNavController mNavController;
+
+    //Better convention to properly name the indices what they are in your app
+    private final int INDEX_RECENTS = FragNavController.TAB1;
+    private final int INDEX_FAVORITES = FragNavController.TAB2;
+    private final int INDEX_NEARBY = FragNavController.TAB3;
+    private final int INDEX_FRIENDS = FragNavController.TAB4;
+    private final int INDEX_FOOD = FragNavController.TAB5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements BaseFragment.Frag
         fragments.add(FriendsFragment.newInstance(0));
         fragments.add(FoodFragment.newInstance(0));
 
-        mNavController = new NavController(getSupportFragmentManager(),R.id.container,fragments);
+        mNavController = new FragNavController(getSupportFragmentManager(),R.id.container,fragments);
 
         mBottomBar = BottomBar.attach(this, savedInstanceState);
         mBottomBar.setItemsFromMenu(R.menu.menu_bottombar, new OnMenuTabClickListener() {
@@ -37,19 +44,19 @@ public class MainActivity extends AppCompatActivity implements BaseFragment.Frag
             public void onMenuTabSelected(@IdRes int menuItemId) {
                 switch (menuItemId) {
                     case R.id.bb_menu_recents:
-                        mNavController.switchTab(NavController.TAB1);
+                        mNavController.switchTab(INDEX_RECENTS);
                         break;
                     case R.id.bb_menu_favorites:
-                        mNavController.switchTab(NavController.TAB2);
+                        mNavController.switchTab(INDEX_FAVORITES);
                         break;
                     case R.id.bb_menu_nearby:
-                        mNavController.switchTab(NavController.TAB3);
+                        mNavController.switchTab(INDEX_NEARBY);
                         break;
                     case R.id.bb_menu_friends:
-                        mNavController.switchTab(NavController.TAB4);
+                        mNavController.switchTab(INDEX_FRIENDS);
                         break;
                     case R.id.bb_menu_food:
-                        mNavController.switchTab(NavController.TAB5);
+                        mNavController.switchTab(INDEX_FOOD);
                         break;
                 }
             }

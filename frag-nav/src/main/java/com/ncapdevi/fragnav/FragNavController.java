b6b351fa3e6b332418d046cf07017322def141d7
Ashok Varma <ASHOKVITECE@GMAIL.COM>
2016-07-01 09:15:51 +0530

    SavedInstant state issues fix
    Handle Case where savedInstant state might not have fragments and added a method to check fragment size and some logging improvement

--- frag-nav/src/main/java/com/ncapdevi/fragnav/FragNavController.java
+++ frag-nav/src/main/java/com/ncapdevi/fragnav/FragNavController.java
@@ -64,14 +64,15 @@ public class FragNavController {
         }
     }
 
-    public FragNavController(Bundle savedInstanceState, @NonNull FragmentManager fragmentManager, @IdRes int containerId, @NonNull List<Fragment> baseFragments, @Transit int transitionMode){
-        this(savedInstanceState, fragmentManager,containerId,baseFragments);
+    public FragNavController(Bundle savedInstanceState, @NonNull FragmentManager fragmentManager, @IdRes int containerId, @NonNull List<Fragment> baseFragments, @Transit int transitionMode) {
+        this(savedInstanceState, fragmentManager, containerId, baseFragments);
         mTransitionMode = transitionMode;
     }
 
     @IntDef({FragmentTransaction.TRANSIT_NONE, FragmentTransaction.TRANSIT_FRAGMENT_OPEN, FragmentTransaction.TRANSIT_FRAGMENT_CLOSE, FragmentTransaction.TRANSIT_FRAGMENT_FADE})
     @Retention(RetentionPolicy.SOURCE)
-    private @interface Transit {}
+    private @interface Transit {
+    }
 
     public void setNavListener(NavListener navListener) {
         mNavListener = navListener;
@@ -86,8 +87,9 @@ public class FragNavController {
     public void switchTab(@TabIndex int index) {
         //Check to make sure the tab is within range
         if (index >= mFragmentStacks.size()) {
-            throw new IndexOutOfBoundsException("Can't switch to a tab that hasn't been initialized. " +
-                    "Make sure to create all of the tabs you need in the Constructor");
+            throw new IndexOutOfBoundsException("Can't switch to a tab that hasn't been initialized, " +
+                    "Index : " + index + ", current stack size : " + mFragmentStacks.size() +
+                    ". Make sure to create all of the tabs you need in the Constructor");
         }
         if (mSelectedTabIndex != index) {
             mSelectedTabIndex = index;
@@ -324,7 +326,7 @@ public class FragNavController {
      * Restores this instance to the state specified by the contents of savedInstanceState
      *
      * @param savedInstanceState The bundle to restore from
-     * @param baseFragments List of base fragments from which to initialize empty stacks
+     * @param baseFragments      List of base fragments from which to initialize empty stacks
      */
     private void onRestoreFromBundle(Bundle savedInstanceState, List<Fragment> baseFragments) {
 
@@ -391,8 +393,20 @@ public class FragNavController {
                 mFragmentStacks.add(stack);
             }
         } catch (Throwable t) {
-            // Nothing we can do
+            mFragmentStacks.clear();
+            for (Fragment fragment : baseFragments) {
+                Stack<Fragment> stack = new Stack<>();
+                stack.add(fragment);
+                mFragmentStacks.add(stack);
+            }
         }
+    }
+
+    public int getSize() {
+        if (mFragmentStacks == null) {
+            return 0;
+        }
+        return mFragmentStacks.size();
     }
 
     public Stack<Fragment> getCurrentStack() {

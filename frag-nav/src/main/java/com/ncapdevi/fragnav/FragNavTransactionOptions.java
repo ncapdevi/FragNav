313daa96cb49e411ccb05d891a36b51a54f47a22
Nic Capdevila <nic@rockbot.com>
2017-02-12 17:12:17 -0800

    - add a builder to the TransactionOptions

--- frag-nav/src/main/java/com/ncapdevi/fragnav/FragNavTransactionOptions.java
+++ frag-nav/src/main/java/com/ncapdevi/fragnav/FragNavTransactionOptions.java
@@ -8,7 +8,7 @@ import android.view.View;
 import java.util.List;
 
 /**
- * Created by niccapdevila on 2/12/17.
+ *
  */
 
 
@@ -22,4 +22,57 @@ public class FragNavTransactionOptions {
     Integer exitAnimationId;
     @StyleRes
     Integer transitionStyle;
+
+    private FragNavTransactionOptions(Builder builder) {
+        sharedElements = builder.sharedElements;
+        transition = builder.transition;
+        enterAnimationId = builder.enterAnimationId;
+        exitAnimationId = builder.exitAnimationId;
+        transitionStyle = builder.transitionStyle;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private List<Pair<View, String>> sharedElements;
+        private Integer transition;
+        private Integer enterAnimationId;
+        private Integer exitAnimationId;
+        private Integer transitionStyle;
+
+        private Builder() {
+        }
+
+        public Builder sharedElements(List<Pair<View, String>> val) {
+            sharedElements = val;
+            return this;
+        }
+
+        public Builder transition(@FragNavController.Transit Integer val) {
+            transition = val;
+            return this;
+        }
+
+        public Builder enterAnimationId(@AnimRes Integer val) {
+            enterAnimationId = val;
+            return this;
+        }
+
+        public Builder exitAnimationId(@AnimRes Integer val) {
+            exitAnimationId = val;
+            return this;
+        }
+
+        public Builder transitionStyle(@StyleRes Integer val) {
+            transitionStyle = val;
+            return this;
+        }
+
+        public FragNavTransactionOptions build() {
+            return new FragNavTransactionOptions(this);
+        }
+    }
 }
+

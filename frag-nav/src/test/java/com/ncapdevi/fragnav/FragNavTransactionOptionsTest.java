d8945d95cdd8237c5cf10ca2df9213f748abbcd6
Nic Capdevila <nic@rockbot.com>
2017-02-25 16:37:55 -0800

    - adding tests to transactionoptions

--- frag-nav/src/test/java/com/ncapdevi/fragnav/FragNavTransactionOptionsTest.java
+++ frag-nav/src/test/java/com/ncapdevi/fragnav/FragNavTransactionOptionsTest.java
@@ -1,5 +1,8 @@
 package com.ncapdevi.fragnav;
 
+import android.support.annotation.AnimRes;
+import android.support.annotation.StyleRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.util.Pair;
 import android.view.View;
 
@@ -15,11 +18,44 @@ public class FragNavTransactionOptionsTest {
 
     @Test
     public void buildTransactionOptions() {
+        String breadCrumbShortTitle = "Short Title";
+        String breadCrumbTitle = "Long Title";
+
+        @AnimRes
+        int enterAnim = 1;
+        @AnimRes
+        int exitAnim = 2;
+        @AnimRes
+        int popEnterAnim = 3;
+        @AnimRes
+        int popExitAnim = 4;
+
+        @StyleRes
+        int transitionStyle = 5;
+
         FragNavTransactionOptions fragNavTransactionOptions = FragNavTransactionOptions.newBuilder()
-                .breadCrumbShortTitle("Short Title")
-                .breadCrumbTitle("Long Title")
+                .breadCrumbShortTitle(breadCrumbShortTitle)
+                .breadCrumbTitle(breadCrumbTitle)
+                .transition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .customAnimations(enterAnim, exitAnim, popEnterAnim, popExitAnim)
+                .transitionStyle(transitionStyle)
                 .addSharedElement(new Pair<View, String>(null, "test"))
                 .addSharedElement(new Pair<View, String>(null, "test2")).build();
+
+        assertTrue(breadCrumbShortTitle.equalsIgnoreCase(fragNavTransactionOptions.breadCrumbShortTitle));
+        assertTrue(breadCrumbTitle.equalsIgnoreCase(fragNavTransactionOptions.breadCrumbTitle));
+
+        assertTrue(transitionStyle == fragNavTransactionOptions.transitionStyle);
+
+        assertTrue(FragmentTransaction.TRANSIT_FRAGMENT_FADE == fragNavTransactionOptions.transition);
+
+
+        assertTrue(enterAnim == fragNavTransactionOptions.enterAnimation);
+        assertTrue(exitAnim == fragNavTransactionOptions.exitAnimation);
+        assertTrue(popEnterAnim == fragNavTransactionOptions.popEnterAnimation);
+        assertTrue(popExitAnim == fragNavTransactionOptions.popExitAnimation);
+
+
 
         assertTrue(fragNavTransactionOptions.sharedElements.size() == 2);
 

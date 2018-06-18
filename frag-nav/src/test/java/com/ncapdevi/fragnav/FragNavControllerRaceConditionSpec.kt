8f33e3c70a5aa858664cd84b1ce351a76cecc808
Mate Herber <mate.herber@skyscanner.net>
2018-06-18 11:57:08 +0200

    Updated Versions: - Kotlin - Gradle - Test FWs

--- frag-nav/src/test/java/com/ncapdevi/fragnav/FragNavControllerRaceConditionSpec.kt
+++ frag-nav/src/test/java/com/ncapdevi/fragnav/FragNavControllerRaceConditionSpec.kt
@@ -7,9 +7,6 @@ import org.jetbrains.spek.api.dsl.on
 
 /**
  * To be able to run these tests in AS one have to install Spek Idea plugin.
- * Currently AS 3+ is buggy so a build found in https://github.com/raniejade/spek-idea-plugin/issues/64
- * thread can be installed and is working properly (gradlew :frag-nav:testDebugUnitTest works nonetheless):
- * https://drive.google.com/file/d/11BT-2alz6C6XxqZCAqggje1l1HvNxarl/view?usp=sharing
  */
 object FragNavControllerRaceConditionSpec : Spek({
     given("A fragment navigation controller with two tabs (A, B) starting on A tab") {
